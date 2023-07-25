c1ef1277c9fc05080a2e4864c1b5ae3f60235fac
Danil Siman <simandanil@gmail.com>
2023-07-26 00:17:09 +0300

    1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,21 @@
-﻿sbyte a = -99;
-ushort b = 55555;
-uint c = 2394672295;
-double d = 65.46;
-char e = 'a';
-string f = "World";
+﻿int result = 4 + 6;
+Console.WriteLine("4 плюс 6 = " + result);
+
+int result2 = 4;
+result2--;
+Console.WriteLine("4 уменьшенное на 1 = " + result2 );
+
+int result3 = 3 * 7;
+Console.WriteLine("3 умножить на 7 = " + result3);
+
+int result4 = 16 / 8;
+Console.WriteLine("16 разделить на 8 = " + result4);
+
+float result5 = Convert.ToSingle(11) / 5;
+Console.WriteLine("11 разделить на 5 = " + result5);
+
+int result6 = 13 % 3;
+Console.WriteLine("целая часть от деления 13 на 3 = " + result6);
+
+float result7 = Convert.ToSingle(17) % 8;
+Console.WriteLine("остаток от деления 17 на 8 = " + result7);
