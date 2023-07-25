351226aa3ddab31db6e9a67ef18fcfb896ec33df
Danil Siman <simandanil@gmail.com>
2023-07-25 23:40:52 +0300

    1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,20 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-string name;
-int age;
-int programCount;
-int money;
-int time;
-bool truth;
-
-name = "Данил";
-age = 25;
-programCount = 2;
-money = 300000;
-time = 1;
-truth = true;
-
-Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
-Console.WriteLine("Мне " + age + " лет.");
-Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
-Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунду.");
-Console.WriteLine("Достигну ли я желаемого ? " + truth + "!");
+﻿sbyte a = -99;
+ushort b = 55555;
+uint c = 2394672295;
+double d = 65.46;
+char e = 'a';
+string f = "World";
