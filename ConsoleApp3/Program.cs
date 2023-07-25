3e228212312a038ac1d3b269a2199a39786613e7
siman <siman@DESKTOP-2PJS4P3>
2023-07-25 19:37:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name;
+int age;
+int programCount;
+int money;
+int time;
+bool truth;
+
+name = "Данил";
+age = 25;
+programCount = 2;
+money = 300000;
+time = 1;
+truth = true;
+
+Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+Console.WriteLine("Мне " + age + " лет.");
+Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
+Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунду.");
+Console.WriteLine("Достигну ли я желаемого ? " + truth + "!");
